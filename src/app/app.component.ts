a6dbdc6819affba542096f942ed6d60a908c7baa
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-07-24 20:36:37 +0300

    Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,43 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<app-password-strength-tester></app-password-strength-tester>',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    password: string = '';
+    showPassword: boolean = false;
+
+    getPasswordStrength(password: string): string {
+        if (password.length === 0) {
+            return 'сірий';
+        }
+
+        if (password.length < 8) {
+            return 'простий';
+        }
+
+        const hasLetters = /[a-zA-Z]/.test(password);
+        const hasSymbols = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(password);
+        const hasNumbers = /\d/.test(password);
+
+        if ((hasLetters || hasSymbols || hasNumbers) && !(hasLetters && hasSymbols && hasNumbers)) {
+            return 'середній';
+        }
+
+        if (hasLetters && hasSymbols && hasNumbers && password.length >= 10) {
+            return 'надійний';
+        }
+
+        return 'простий';
+    }
+
+    calculatePasswordStrength(password: string) {
+        this.password = password;
+    }
+
+    toggleShowPassword() {
+        this.showPassword = !this.showPassword;
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { PasswordStrengthTesterComponent } from './password-strength-tester/password-strength-tester.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PasswordStrengthTesterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
